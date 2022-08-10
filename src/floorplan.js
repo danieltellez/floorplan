a36b9871bfa330b702e7aba336e5b15400351d45
Daniel Tellez <danieltellez@flossfriday.com>
2022-08-10 13:04:58 +0200

    enhance rotation tool

--- src/floorplan.js
+++ src/floorplan.js
@@ -627,6 +627,7 @@ d3.floorplan = function () {
             )
 
         rotationIcon
+            .style('fill', 'red')
             .append("path")
             .attr("d", "M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z")
             .attr("fill-rule", "evenodd")
@@ -634,6 +635,13 @@ d3.floorplan = function () {
         rotationIcon
             .append("path")
             .attr("d", "M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z")
+
+        rotationIcon
+            .append('circle')
+            .attr('cx', 8)
+            .attr('cy', 8)
+            .attr('r', '10px')
+            .attr('class', 'tool')
 
         // Set translate variable defaults;
         gSensorContainer.datum({
